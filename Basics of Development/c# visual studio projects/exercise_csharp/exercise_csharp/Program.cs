a49a350362b7dadce5ed86b12e75644fe435f2e8
Zdravko Stanin <61220445+zdravkostanin1@users.noreply.github.com>
2022-11-24 20:19:38 +0200

    Add files via upload

--- Basics of Development/c# visual studio projects/exercise_csharp/exercise_csharp/Program.cs
+++ Basics of Development/c# visual studio projects/exercise_csharp/exercise_csharp/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int read = int.Parse(Console.ReadLine());
+int[] arr = new int[read];
+int sum = 0;
+
+for(int i = 0; i < arr.Length; i++)
+{
+    int read2 = int.Parse(Console.ReadLine());
+    arr[i] = read2;
+    Console.WriteLine(arr);
+}
+
+// SUM OF ARRAYS:
+int[] a = { 2, 5, 6, 8, 23 };
+int[] b = { 99, 34, -2, 3, 4, 7, 8 };
+int[] c = new int[b.Length];
+
+for(int i = 0; i < c.Length; i++)
+{
+    if (i < a.Length)
+    {
+        c[i] = a[i] + b[i];
+    }
+    else
+    {
+        c[i] = b[i];
+    }
+}
